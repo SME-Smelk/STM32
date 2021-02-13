6295ba95a786cd771a333a07a119e484cce95aca
ismael <ismael.pbt@gmail.com>
2021-02-13 20:39:07 -0300

    Add header and message for 01-UART2_HelloWorld in main.c

--- Boards Reference code/HAL/STM32F4DISCOVERY - F407VG/01-UART2_HelloWord/Core/Src/main.c
+++ Boards Reference code/HAL/STM32F4DISCOVERY - F407VG/01-UART2_HelloWord/Core/Src/main.c
@@ -1,22 +1,17 @@
 /**
   ******************************************************************************
-  * @Project        : 00-BASE-Basic
+  * @Project        : 01-UART2_HelloWorld
   * @Autor          : Ismael Poblete
   * @Company		: -
-  * @Date         	: 6-28-2020
-  * @brief          : Archivo base para crear programaciones.
+  * @Date         	: 13-02-2021
+  * @brief          : Basic use HAL for UART2, need uart-ttl converter.
   * @System Clock
-  * 	SYSSource:		HSI/HSE/HSI-PLL/HSE-PLL (Por defecto HSI)
-  * 	SYSCLK: 		XMHz					(Por defecto 8MHz)
+  * 	SYSSource:		HSE-PLL
+  * 	SYSCLK: 		84MHz
   * @Perf
   * 	*UART2
-  * 		PA2			<-----> USART_TX
-  * 		PA3			<-----> USART_RX
-  * 	*GPIO
-  * 		PA5      	------> LD2
-  * 		PC13     	<------ B1(Button)
-  * 	*ADC
-  * 		PA0    	 	------> ADC1,INO
+  * 		PA2			<-----> USART2_TX
+  * 		PA3			<-----> USART2_RX
   ******************************************************************************
 **/
 
@@ -75,9 +70,13 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
 
-  /** Initializes the CPU, AHB and APB busses clocks
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
@@ -85,29 +84,22 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = 8;
   RCC_OscInitStruct.PLL.PLLN = 336;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
   RCC_OscInitStruct.PLL.PLLQ = 7;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
   }
-  /** Initializes the CPU, AHB and APB busses clocks
+  /** Initializes the CPU, AHB and APB buses clocks
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_I2S;
-  PeriphClkInitStruct.PLLI2S.PLLI2SN = 192;
-  PeriphClkInitStruct.PLLI2S.PLLI2SR = 2;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
