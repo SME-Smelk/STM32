2affe784c739797e1e50c75ada6012f0836048a6
ismael <ismael.pbt@gmail.com>
2021-02-13 22:59:14 -0300

    03-UART2_TXRX_IT added

---
+++ Boards Reference code/HAL/STM32F4DISCOVERY - F407VG/03-UART2_TXRX_IT/Core/Src/main.c
@@ -0,0 +1,199 @@
+/**
+  ******************************************************************************
+  * @Project        : 03-UART2_TXRX_IT
+  * @Autor          : Ismael Poblete
+  * @Company		: -
+  * @Date         	: 02-13-2020
+  * @brief          : Receive data via IT and form the word(data) with \n in the end,
+  * 				  then when it was ready send data. Need a uart to tll converter
+  * @Lib			: CMSIS, HAL.
+  * @System Clock
+  * 	SYSSource:		HSE-PLL
+  * 	SYSCLK: 		84MHz
+  * @Perf
+  * 	*UART2
+  * 		PA2			<-----> USART_TX
+  * 		PA3			<-----> USART_RX
+  ******************************************************************************
+**/
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+
+/* Private typedef -----------------------------------------------------------*/
+UART_HandleTypeDef huart2;
+
+/* Private define ------------------------------------------------------------*/
+
+/* Private macro -------------------------------------------------------------*/
+
+/* Private variables ---------------------------------------------------------*/
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void USART2_UART_Init(void);
+
+/* Private user code ---------------------------------------------------------*/
+char *user_data = "The application is running\r\n";
+uint8_t data_buffer[100];
+uint8_t recvd_data;
+uint32_t count=0;
+uint8_t reception_complete = FALSE;
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
+
+	/* Configure the system clock */
+	SystemClock_Config();
+
+	/* Initialize all configured peripherals */
+	USART2_UART_Init();
+
+	/* User Code */
+	uint16_t len_of_data = strlen(user_data);
+	HAL_UART_Transmit(&huart2,(uint8_t*)user_data,len_of_data,HAL_MAX_DELAY);
+	HAL_UART_Receive_IT(&huart2,&recvd_data,1);
+
+	/* Start Code */
+	while (1)
+	{
+		/* If the interrupts formed the word then send via UART2*/
+		if(reception_complete == TRUE){
+			HAL_UART_Transmit(&huart2,data_buffer,count,HAL_MAX_DELAY);
+			count=0;
+			memset(data_buffer, 0, sizeof(data_buffer));
+			reception_complete = FALSE;
+		}
+	}
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+
+  /** Initializes the CPU, AHB and APB busses clocks
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB busses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_I2S;
+  PeriphClkInitStruct.PLLI2S.PLLI2SN = 192;
+  PeriphClkInitStruct.PLLI2S.PLLI2SR = 2;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void USART2_UART_Init(void)
+{
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief CallBack UART
+  * @param None
+  * @retval None
+  */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+
+	 if(recvd_data == '\n')
+	 {
+		 reception_complete = TRUE;
+		 data_buffer[count++]='\r';
+		 data_buffer[count++]='\n';
+		 /* for inmediate responce
+		 HAL_UART_Transmit(huart,data_buffer,count,HAL_MAX_DELAY);
+		 count=0;
+		 memset(data_buffer, 0, sizeof(data_buffer))
+		  * */
+	 }
+	 else
+	 {
+		 data_buffer[count++] = recvd_data;
+	 }
+
+	 HAL_UART_Receive_IT(&huart2,&recvd_data,1);
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* User can add his own implementation to report the HAL error return state */
+
+
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+}
+#endif /* USE_FULL_ASSERT */
+
+
