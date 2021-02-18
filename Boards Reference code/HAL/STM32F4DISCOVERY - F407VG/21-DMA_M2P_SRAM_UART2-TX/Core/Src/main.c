00175c4c876c5112a7b9e79db57fe14988f959cc
Ismael Poblete (H) <ismael.pbt@gmail.com>
2021-02-18 15:29:06 -0300

    Update Sysclock information for 19,20 and 21. Project 22 added.

--- Boards Reference code/HAL/STM32F4DISCOVERY - F407VG/21-DMA_M2P_SRAM_UART2-TX/Core/Src/main.c
+++ Boards Reference code/HAL/STM32F4DISCOVERY - F407VG/21-DMA_M2P_SRAM_UART2-TX/Core/Src/main.c
@@ -9,7 +9,7 @@
   * 				  See data via window "Memory Browser"
   * @Lib			: CMSIS, HAL.
   * @System Clock
-  * 	SYSSource:		HSE
+  * 	SYSSource:		PLL (HSE)
   * 	SYSCLK: 		84MHz
   * @Perf
   * 	*UART2
