93606a9d95a520bd0dd79ef7c5b9bc8530eed30f
Ismael Poblete (H) <ismael.pbt@gmail.com>
2021-02-23 19:30:34 -0300

    Project FreeRTOS_00-Task-SemiHosting

---
+++ Boards Reference code/HAL/STM32F4DISCOVERY - F407VG/FreeRTOS_00-Task-SemiHosting/Core/Src/stm32f4xx_hal_msp.c
@@ -0,0 +1,36 @@
+/**
+  ******************************************************************************
+  * File Name          : stm32f4xx_hal_msp.c
+  * Description        : This file provides code for the MSP Initialization 
+  *                      and de-Initialization codes.
+  ******************************************************************************
+*/
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private typedef -----------------------------------------------------------*/
+
+/* Private define ------------------------------------------------------------*/
+
+/* Private macro -------------------------------------------------------------*/
+
+/* Private variables ---------------------------------------------------------*/
+
+/* Private function prototypes -----------------------------------------------*/
+
+/* External functions --------------------------------------------------------*/
+
+
+/**
+  * Initializes the Global MSP.
+  */
+
+void HAL_MspInit(void)
+{
+
+  /* System interrupt init*/
+	HAL_NVIC_SetPriority(PendSV_IRQn, 15, 0);
+
+}
+
