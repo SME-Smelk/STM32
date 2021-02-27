cd82f7a502bf4cafed1ed9cb0aae6010c2a426d2
Ismael Poblete (H) <ismael.pbt@gmail.com>
2021-02-27 12:27:58 -0300

    Clean projects, debug and launch files folders deleted.

--- Boards Reference code/HAL/STM32F4DISCOVERY - F407VG/FreeRTOS_03-LedButton/Debug/Third-Party/SEGGER/SEGGER/SEGGER_RTT.d
+++
@@ -1,8 +0,0 @@
-Third-Party/SEGGER/SEGGER/SEGGER_RTT.o: \
- ../Third-Party/SEGGER/SEGGER/SEGGER_RTT.c \
- ../Third-Party/SEGGER/SEGGER/SEGGER_RTT.h \
- C:/Users/ismae/Dropbox/Smelk/Universidad\ y\ Conocimiento/Cursos/Electronica/Mastering\ RTOS\ Hands-on\ FreeRTOS\ and\ STM32Fx\ with\ Debugging(RTOS)/SME/Programaciones/06-FreeRTOS-LedButton/Third-Party/SEGGER/Config/SEGGER_RTT_Conf.h
-
-../Third-Party/SEGGER/SEGGER/SEGGER_RTT.h:
-
-C:/Users/ismae/Dropbox/Smelk/Universidad\ y\ Conocimiento/Cursos/Electronica/Mastering\ RTOS\ Hands-on\ FreeRTOS\ and\ STM32Fx\ with\ Debugging(RTOS)/SME/Programaciones/06-FreeRTOS-LedButton/Third-Party/SEGGER/Config/SEGGER_RTT_Conf.h:
--- Boards Reference code/HAL/STM32F4DISCOVERY - F407VG/FreeRTOS_13-Mutex-BinSema/Debug/Third-Party/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.d
+++
@@ -1,8 +0,0 @@
-Third-Party/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.o: \
- ../Third-Party/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.S \
- ../Third-Party/SEGGER/SEGGER/SEGGER_RTT.h \
- C:/Users/ismae/Dropbox/Smelk/Universidad\ y\ Conocimiento/Cursos/Electronica/Mastering\ RTOS\ Hands-on\ FreeRTOS\ and\ STM32Fx\ with\ Debugging(RTOS)/SME/Programaciones/16-FreeRTOS-Mutex-BinSema/Third-Party/SEGGER/Config/SEGGER_RTT_Conf.h
-
-../Third-Party/SEGGER/SEGGER/SEGGER_RTT.h:
-
-C:/Users/ismae/Dropbox/Smelk/Universidad\ y\ Conocimiento/Cursos/Electronica/Mastering\ RTOS\ Hands-on\ FreeRTOS\ and\ STM32Fx\ with\ Debugging(RTOS)/SME/Programaciones/16-FreeRTOS-Mutex-BinSema/Third-Party/SEGGER/Config/SEGGER_RTT_Conf.h:
