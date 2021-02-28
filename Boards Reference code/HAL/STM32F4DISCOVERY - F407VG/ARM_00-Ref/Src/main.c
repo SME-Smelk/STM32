11a9d107a46c5fb941509a0c2d0f9097572b5687
Ismael Poblete (H) <ismael.pbt@gmail.com>
2021-02-27 21:28:18 -0300

    Base projects ARM_0-16 added.

---
+++ Boards Reference code/HAL/STM32F4DISCOVERY - F407VG/ARM_00-Ref/Src/main.c
@@ -0,0 +1,30 @@
+/**
+  ******************************************************************************
+  * @Project        : ARM_00-Ref
+  * @Autor          : Ismael Poblete
+  * @Company		: -
+  * @Date         	: 02-27-2021
+  * @Target			: DISCOVERY-DISC1 STM32F407VG
+  * @brief          : Base programming guide
+  * @Lib			: -
+  * @System Clock
+  * 	SYSSource:		HSI/HSE/PLL(HSI)/PLL(HSE) 	(Ej: HSI)
+  * 	SYSCLK: 		XMHz						(EJ: 8MHz)
+  * 	RTCSource:		None/LSI/LSE/HSE-DIVx 			(Ej: LSI)
+  * 	RTCCLK: 		None/XMHz						(EJ: 32kHz)
+  * @Perf
+  * 	*UART2
+  * 		PA2			<-----> USART_TX
+  * 		PA3			<-----> USART_RX
+  *
+  ******************************************************************************
+**/
+
+#include <stdio.h>
+
+
+int main(void)
+{
+
+	for(;;);
+}
