94760af582b3511ecefba67e53906800f34b160e
Ismael Poblete (H) <ismael.pbt@gmail.com>
2021-02-28 00:05:36 -0300

    Project ARM_00_02 completed.

--- Boards Reference code/HAL/STM32F4DISCOVERY - F407VG/ARM_03-Inline/Src/main.c
+++ Boards Reference code/HAL/STM32F4DISCOVERY - F407VG/ARM_03-Inline/Src/main.c
@@ -10,12 +10,10 @@
 int main(void)
 {
 
-
-//Parte 1. Uso de ensamblador y inline
-//Carga dos valores de la memoria y sumalos y alamacenalos en la memoria usando inline assembly
-
 #if 0
-
+/*Part 1. Use assembly and inline
+* Load two values in the memory, add them and save in memory with inline assembly
+*/
 	//Inicializo direccion en R1
 	__asm volatile("LDR R1,=#0x20001000");
 	//Inicializo direccion en R2
@@ -32,11 +30,12 @@ int main(void)
 	__asm volatile("ADD R0,R0,R1"); //ADD 1,2,3
 	//Almaceno valor de la memoria utilizada R0 a R2
 	__asm volatile("STR R0,[R2]");
+
 #endif
 
-//Parte 2. Uso de ensamblador con expresiones de C, Uso de constraint.
-//Lectura de registro especial CONTROL, escritura en de variable c en R0, lectura de puntero.
-
+/*Part 2. Use of assembly with C expressions and constraint.
+* Read the especial register CONTROL, write variable C in R0, pointer read.
+*/
 	// Almaceno variable val en registro R0
 	int val=50;
 	__asm volatile("MOV R0,%0": :"r"(val));
