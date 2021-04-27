36006fc10afcd278802ffff3a37c4acbe51f1ce2
Ismael Poblete V. <ismael.pbt@gmail.com>
2021-04-27 12:33:33 -0400

    NUCLEO-F103RB AD7893-1_00 project added.

---
+++ Boards Reference code/HAL/NUCLEO-F103RB/AD7893-1_00_Simple50KHz/Core/Src/main.c
@@ -0,0 +1,369 @@
+/**
+  ******************************************************************************
+  * @Project        : AD7893-1_00_Simple50KHz
+  * @Autor          : Ismael Poblete V.
+  * @Company		: -
+  * @Date         	: 04-27-2021
+  * @Target			: DISCOVERY-DISC1 STM32F407VG
+  * @brief          : Simple ADC AD7891-1 SPI Serial Communication
+  * @Lib			: CMSIS, HAL.
+  * @System Clock
+  * 	SYSSource:		PLL(HSI)
+  * 	SYSCLK: 		64MHz
+  * 	RTCSource:		None
+  * 	RTCCLK: 		None
+  * @Perf
+  * 	*UART2
+  * 		PA2			<-----> USART_TX
+  * 		PA3			<-----> USART_RX
+  * 	*GPIO
+  * 		PA5      	------> LED
+  * 	*TIMER2 (Software 50khz - 20us)
+  * 	*SPI-ADC 16MHz
+  *			PB14	->	SDATA (SPI2_MISO)
+  *			PB13	->	SCLK  (SPI2_SCK)
+  * 		PB2		->	RFS
+  * 		PB11	->	EOC
+  * 		PB1		->	CONVST
+  *
+  * @note
+  * 	-Data acquisition for AD7892ANZ-1 with SPI serial communication mode.
+  * 	-Timer as a 50Khz - 20us for data acquisition.
+  * 	-LD2 Led to observe the interrupt timer transition
+  *
+  *		####################
+  * 	AD7892-1 Pins
+  * 	####################
+  *
+		1 VDD 					- +5V
+		2  ~STANDBY 			- +5V
+		3 Vin2 					- GNDA
+		4 Vin1 			 		- Input-Signal
+		5 REF OUT/REF IN 		- Capacitor 0.1 uF to GND
+		6 AGND 					- GNDA
+		7 MODE 					- GNDD
+		8 LOW 					- GNDD
+		9 LOW 					- GNDD
+		10,11,12,13 			- NC
+		14 DGND 				- GNDD
+		15 SDATA 				- MISO
+		16 SCLK 				- SCK
+		17 ~RFS 				- RFS
+		18,19,20 				- NC
+		21 ~RD 					- NC
+		22 ~CS 					- NC
+		23 ~EOC 				- EOC
+		24 ~CONVST 				- CONVST
+  *
+  *		######################
+  * 	Data Clock Form.
+  * 	######################
+  *
+  *		SDATA	__________..___________DATA_#############_____..___ (0Xfe)
+  *		SCLK	############_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_######## (16MHz)
+  *		~RFS	############________________________________####### (12.54us)
+  *		~EOC	########__#########################################	(85ns)
+  *		~CONVST	###__############################################## (140us)
+  *
+  ******************************************************************************
+**/
+
+/* Includes ------------------------------------------------------------------*/
+
+#include "main.h"
+#include "stdio.h"
+#include "string.h"
+
+/* Private includes ----------------------------------------------------------*/
+
+/* Private typedef -----------------------------------------------------------*/
+
+/* Private define ------------------------------------------------------------*/
+
+/* Private macro -------------------------------------------------------------*/
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart2;
+SPI_HandleTypeDef hspi2;
+TIM_HandleTypeDef htim2;
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void GPIO_Init(void);
+static void USART2_UART_Init(void);
+static void SPI2_Init(void);
+static void TIM2_Init(void);
+/* Private user code ---------------------------------------------------------*/
+
+char msg[100];
+uint8_t spidata = 0xFA;
+uint16_t spi_receive_data;
+uint16_t data =0;
+float data_adc = 0.0;
+#define K_PARAMETER 20.0 / 4096
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* Initialize all configured peripherals */
+  GPIO_Init();
+  USART2_UART_Init();
+  SPI2_Init();
+  TIM2_Init();
+  HAL_TIM_Base_Start_IT(&htim2);
+
+  AD7892_Init();
+
+  /* Infinite loop */
+  while (1)
+  {
+	  /* Test the interrupt cycle - RESET*/
+	  LD2_GPIO_Port->BSRR = (uint32_t)LD2_Pin << 16u;
+
+  }
+}
+
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void USART2_UART_Init(void)
+{
+
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
+
+/**
+  * @brief SPI2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void SPI2_Init(void)
+{
+  /* SPI2 parameter configuration*/
+  hspi2.Instance = SPI2;
+  hspi2.Init.Mode = SPI_MODE_MASTER;
+  hspi2.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi2.Init.DataSize = SPI_DATASIZE_16BIT;
+  hspi2.Init.CLKPolarity = SPI_POLARITY_HIGH;
+  hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi2.Init.NSS = SPI_NSS_SOFT;
+  hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+  hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi2.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+
+static void TIM2_Init(void)
+{
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = TIMER2_PRESCALER;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = TIMER2_PERIOD;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief Timer callback
+  * @param htim
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
+	/* Test the interrupt cycle - SET*/
+	LD2_GPIO_Port->BSRR = LD2_Pin;
+
+	AD7892_Getdata();
+}
+
+void AD7892_Init(void){
+
+	  HAL_GPIO_WritePin(CONVST_GPIO_Port,CONVST_Pin,GPIO_PIN_SET);
+	  HAL_GPIO_WritePin(RFS_GPIO_Port,RFS_Pin,GPIO_PIN_SET);
+
+}
+
+void AD7892_Getdata(void){
+
+	  /* Init
+
+			HCKL 64MHz - APB1 64MHz
+			CLOCK,CONVST,EOC,RFS must be HIGH
+			(*): Must be done by the MCU
+
+			*CONVST: Send pulse for data
+			EOC: After 1.6us, it response with a 85ns pulse (data ready)
+			*RFS: Must be LOW for SPI receive transmission and HIGH when it finished.
+
+	  */
+
+	  /* CONVST LOW TOGGLE 140ns (30ns minimum) */
+	  CONVST_GPIO_Port->BSRR = (uint32_t)CONVST_Pin << 16u; //RESET
+	  CONVST_GPIO_Port->BSRR = CONVST_Pin;					//SET
+
+	  /* Wait to EOC response (1.6us) and it will be a toggle LOW 85ns.
+	    Wait to pulse ends 1.56us */
+	  for(uint32_t i = 0; i < 6; i++);
+
+	  /* Start transmission Set RFS LOW - RESET PIN */
+	  RFS_GPIO_Port->BSRR = (uint32_t)RFS_Pin << 16u;
+
+	  /* Receive data SPI */
+	  HAL_SPI_Receive(&hspi2,(uint8_t*)&spi_receive_data,1,1);
+
+	  /* Finish of transmission Set RFS HIGH - SET PIN*/
+	  RFS_GPIO_Port->BSRR = RFS_Pin;
+
+}
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(RFS_GPIO_Port, RFS_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(CONVST_GPIO_Port, CONVST_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : ASD_Pin */
+  GPIO_InitStruct.Pin = EOC_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(EOC_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : ASD_Pin */
+  GPIO_InitStruct.Pin = CONVST_Pin | RFS_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(CONVST_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  __disable_irq();
+  while (1)
+  {
+  }
+}
---
+++ Boards Reference code/HAL/NUCLEO-F103RB/AD7893-1_00_Simple50KHz/Core/Src/stm32f1xx_hal_msp.c
@@ -0,0 +1,142 @@
+/**
+  ******************************************************************************
+  * @file         stm32f1xx_hal_msp.c
+  * @brief        This file provides code for the MSP Initialization
+  *               and de-Initialization codes.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private typedef -----------------------------------------------------------*/
+
+/* Private define ------------------------------------------------------------*/
+
+/* Private macro -------------------------------------------------------------*/
+
+/* Private variables ---------------------------------------------------------*/
+
+/* Private function prototypes -----------------------------------------------*/
+
+/* External functions --------------------------------------------------------*/
+
+
+/**
+  * Initializes the Global MSP.
+  */
+void HAL_MspInit(void)
+{
+
+
+}
+
+/**
+* @brief UART MSP Initialization
+* This function configures the hardware resources used in this example
+* @param huart: UART handle pointer
+* @retval None
+*/
+void HAL_UART_MspInit(UART_HandleTypeDef* huart)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(huart->Instance==USART2)
+  {
+
+    /* Peripheral clock enable */
+    __HAL_RCC_USART2_CLK_ENABLE();
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**USART2 GPIO Configuration
+    PA2     ------> USART2_TX
+    PA3     ------> USART2_RX
+    */
+    GPIO_InitStruct.Pin = USART_TX_Pin|USART_RX_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    /* USART2 interrupt Init */
+    HAL_NVIC_SetPriority(USART2_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(USART2_IRQn);
+
+  }
+
+}
+
+/**
+* @brief SPI MSP Initialization
+* This function configures the hardware resources used in this example
+* @param hspi: SPI handle pointer
+* @retval None
+*/
+void HAL_SPI_MspInit(SPI_HandleTypeDef* hspi)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(hspi->Instance==SPI2)
+  {
+
+    /* Peripheral clock enable */
+    __HAL_RCC_SPI2_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+
+    /**SPI2 GPIO Configuration
+    PB13     ------> SPI2_SCK
+    PB14     ------> SPI2_MISO
+    PB15     ------> SPI2_MOSI
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_13 | GPIO_PIN_15;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_14;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /* SPI2 interrupt Init */
+    HAL_NVIC_SetPriority(SPI2_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(SPI2_IRQn);
+
+  }
+
+}
+
+/**
+* @brief TIM_Base MSP Initialization
+* This function configures the hardware resources used in this example
+* @param htim_base: TIM_Base handle pointer
+* @retval None
+*/
+void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
+{
+  if(htim_base->Instance==TIM2)
+  {
+    /* Peripheral clock enable */
+    __HAL_RCC_TIM2_CLK_ENABLE();
+    /* TIM2 interrupt Init */
+    HAL_NVIC_SetPriority(TIM2_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(TIM2_IRQn);
+  }
+
+  if(htim_base->Instance==TIM3)
+  {
+    /* Peripheral clock enable */
+    __HAL_RCC_TIM3_CLK_ENABLE();
+    /* TIM2 interrupt Init */
+    HAL_NVIC_SetPriority(TIM3_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(TIM3_IRQn);
+  }
+}
